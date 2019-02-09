0879005ffa04a6bf1f8e2f4980a27b3dc21e8327
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-02-09 05:29:47 -0600

    fix bug

--- lib/config.py
+++ lib/config.py
@@ -2,77 +2,80 @@ import os
 from pathlib import Path
 
 
+def get_windows_config():
+    return {
+        "filtered_data_root":
+        Path("D:/code/data/filtered_datasets/coco_sky_test"),
+        "target_supercategories": ["sky"],
+        "bbox_type":
+        "aggregated",
+        "n_boxes":
+        10,
+        "splits": [
+            {
+                "name":
+                "train",
+                "ann_file":
+                Path("D:/code/data/cocostuff/dataset/annotations/",
+                     "stuff_train2017.json"),
+                "data_root":
+                Path("D:/code/data/cocostuff/dataset/images/train2017"),
+                "fraction":
+                0.1
+            },
+            {
+                "name":
+                "val",
+                "ann_file":
+                Path("D:/code/data/cocostuff/dataset/annotations/",
+                     "stuff_val2017.json"),
+                "data_root":
+                Path("D:/code/data/cocostuff/dataset/images/val2017"),
+                "fraction":
+                0.01
+            },
+        ]
+    }
+
+
+def get_ubuntu_config():
+    return {
+        "filtered_data_root":
+        Path("/mnt/hdd-4tb/testuser2/datasets/coco_sky_weak_bb_agg"),
+        "target_supercategories": ["sky"],
+        "bbox_type":
+        "aggregated",
+        "n_boxes":
+        10,
+        "splits": [
+            {
+                "name":
+                "train",
+                "ann_file":
+                Path("/mnt/hdd-4tb/abhay/cocostuff/dataset/annotations/",
+                     "stuff_train2017.json"),
+                "data_root":
+                Path("/mnt/hdd-4tb/abhay/cocostuff/dataset/images/train2017"),
+                "fraction":
+                0.1
+            },
+            {
+                "name":
+                "val",
+                "ann_file":
+                Path("/mnt/hdd-4tb/abhay/cocostuff/dataset/annotations/",
+                     "stuff_val2017.json"),
+                "data_root":
+                Path("/mnt/hdd-4tb/abhay/cocostuff/dataset/images/val2017"),
+                "fraction":
+                0.01
+            },
+        ]
+    }
+
+
 def get_config():
     if os.name == "nt":
-        return
-        {
-            "filtered_data_root":
-            Path("D:/code/data/filtered_datasets"),
-            "target_supercategories": ["sky"],
-            "bbox_type":
-            "aggregated",
-            "n_boxes":
-            10,
-            "splits": [
-                {
-                    "name":
-                    "train",
-                    "ann_file":
-                    Path("D:/code/data/cocostuff/dataset/annotations/",
-                         "stuff_train2017.json"),
-                    "data_root":
-                    Path("D:/code/data/cocostuff/dataset/images/train2017"),
-                    "fraction":
-                    0.1
-                },
-                {
-                    "name":
-                    "val",
-                    "ann_file":
-                    Path("D:/code/data/cocostuff/dataset/annotations/",
-                         "stuff_val2017.json"),
-                    "data_root":
-                    Path("D:/code/data/cocostuff/dataset/images/val2017"),
-                    "fraction":
-                    0.01
-                },
-            ]
-        }
+        return get_windows_config()
     else:
-        return
-        {
-            "filtered_data_root":
-            Path("/mnt/hdd-4tb/abhay/datasets"),
-            "target_supercategories": ["sky"],
-            "bbox_type":
-            "aggregated",
-            "n_boxes":
-            10,
-            "splits": [
-                {
-                    "name":
-                    "train",
-                    "ann_file":
-                    Path("/mnt/hdd-4tb/abhay/cocostuff/dataset/annotations/",
-                         "stuff_train2017.json"),
-                    "data_root":
-                    Path(
-                        "/mnt/hdd-4tb/abhay/cocostuff/dataset/images/train2017"
-                    ),
-                    "fraction":
-                    0.1
-                },
-                {
-                    "name":
-                    "val",
-                    "ann_file":
-                    Path("/mnt/hdd-4tb/abhay/cocostuff/dataset/annotations/",
-                         "stuff_val2017.json"),
-                    "data_root":
-                    Path(
-                        "/mnt/hdd-4tb/abhay/cocostuff/dataset/images/val2017"),
-                    "fraction":
-                    0.01
-                },
-            ]
-        }
+        return get_ubuntu_config()
