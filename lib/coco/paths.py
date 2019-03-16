0ca47d892751bf9670b5fef6bd63fb7273e89c50
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-03-16 13:03:57 -0500

    setup paths for aws

--- lib/coco/paths.py
+++ lib/coco/paths.py
@@ -53,6 +53,23 @@ def get_leibniz_paths():
     }
 
 
+def get_aws_paths():
+    return {
+        "filtered_data_root":
+        Path("/home/ubuntu/datasets/filtered"),
+        "train_ann_file":
+        Path("/home/ubuntu/datasets/cocostuff/dataset/annotations/",
+             "stuff_train2017.json"),
+        "train_root":
+        Path("/home/ubuntu/datasets/cocostuff/dataset/images/train2017"),
+        "val_ann_file":
+        Path("/home/ubuntu/datasets/cocostuff/dataset/annotations/",
+             "stuff_val2017.json"),
+        "val_root":
+        Path("/home/ubuntu/datasets/cocostuff/dataset/images/val2017"),
+    }
+
+
 def get_paths():
     if platform.node() == "Anton":
         return get_anton_paths()
@@ -60,3 +77,7 @@ def get_paths():
         return get_leibniz_paths()
     elif platform.node() == "tesla":
         return get_tesla_paths()
+    elif platform.node() == "ip-172-31-5-229":
+        return get_aws_paths()
+    else:
+        raise ValueError("Platform not supported")
