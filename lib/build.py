8b0d45075eafc3499cf3a5391c05bb1de4f4e68d
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-05 10:44:14 -0500

    clean

---
+++ lib/build.py
@@ -0,0 +1,2 @@
+def build(config):
+    raise NotImplementedError
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lib.configure import get_config
 import argparse
-import importlib
+import importlib  # noqa F401
 
 
 def get_args():
@@ -14,23 +14,8 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
     config = get_config(args.config_file)
-    if args.mode == "build":
-        importlib.import_module("lib." + config["dataset"] +
-                                ".builder").Builder(config).build()
-    elif args.mode == "verify":
-        if config["dataset"] != "coco":
-            raise NotImplementedError
-
-        analytics = importlib.import_module("lib." + config["dataset"] +
-                                            ".analytics")
-        analytics.verify_images(config)
-    elif args.mode == "histogram":
-        if config["dataset"] != "coco":
-            raise NotImplementedError
-
-        importlib.import_module("lib." + config["dataset"] +
-                                ".analytics").Analyzer(
-                                    config).compute_label_fraction_histogram()
-
+    if args.mode in ["build", "verify", "histogram"]:
+        exec("importlib.import_module(\"lib.{mode}\").{mode}(config)".format(
+            mode=args.mode))
     else:
         raise NotImplementedError("Mode: " + args.mode + " not supported.")
