c9b4903fbbdc28985b800df833363e9068d685af
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-07 05:13:07 -0500

    remove mnist

--- lib/build.py
+++ lib/build.py
@@ -2,7 +2,7 @@ import importlib
 
 
 def build(config):
-    if config["dataset"] in ["coco", "mnist"]:
+    if config["dataset"] in ["coco"]:
         module_name = "datasets." + config["dataset"] + ".lib.builder"
         importlib.import_module(module_name).Builder(config).build()
     else:
