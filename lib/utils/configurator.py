58085b3212fd981e7fbbc31e064be464ba3f3ed1
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-27 06:51:27 -0500

    better configurator

--- lib/utils/configurator.py
+++ lib/utils/configurator.py
@@ -1,15 +1,33 @@
+from pathlib import Path
+import os
 import yaml
 
 
 class Configurator:
     def configure(self, config_file):
+        config = self._load(config_file)
+        config = self._set_defaults(config)
+        config = self._build_paths(config)
+        return config
+
+    def _set_defaults(self, config):
+        if "size" not in config.keys():
+            config["size"] = 640
+        return config
+
+    def _load(self, config_file):
         with open(config_file, 'r') as stream:
             try:
-                return self._set_defaults(yaml.load(stream))
+                config = yaml.load(stream)
+                config["name"] = Path(config_file).stem
+                return config
             except yaml.YAMLError as exc:
                 print(exc)
 
-    def _set_defaults(self, config):
-        if "dataset" not in config.keys():
-            config["dataset"] = "coco"
+    def _build_paths(self, config):
+        config["dataset path"] = Path(config["destination"], config["name"])
+        if os.path.exists(config["dataset path"]):
+            raise RuntimeError("Dataset already exists. ")
+        else:
+            os.mkdir(config["dataset path"])
         return config
