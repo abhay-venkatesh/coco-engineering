d6e7a466657c1d5f06851a5c7d113ed76516a30f
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-16 08:33:24 -0500

    stop for now

---
+++ lib/agents/single_stuff_agent.py
@@ -0,0 +1,14 @@
+from lib.analyzers.analyzer import Analyzer
+from lib.builders.builder import TrainSingleStuffBuilder, ValSingleStuffBuilder
+
+
+class SingleStuffAgent:
+    def run(self, config):
+        dataset = TrainSingleStuffBuilder(config).build()
+        analyzer = Analyzer(config)
+        analyzer.verify(dataset)
+        analyzer.compute_label_fraction_histogram(dataset)
+
+        dataset = ValSingleStuffBuilder(config).build()
+        analyzer = Analyzer(config)
+        analyzer.verify(dataset)
