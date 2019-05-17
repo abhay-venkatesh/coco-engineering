5cc82c0a6eeabbc5d65be423662d394e8a5491f0
abhay-venkatesh <abhay.venkatesh@gmail.com>
2019-05-17 18:09:20 -0500

    stop for now

--- lib/agents/full_stuff_agent.py
+++ lib/agents/full_stuff_agent.py
@@ -1,14 +1,8 @@
-from lib.analyzers.analyzer import Analyzer
 from lib.builders.full_stuff_builder import TrainFullStuffBuilder, \
     ValFullStuffBuilder
 
 
 class FullStuffAgent:
     def run(self, config):
-        dataset = TrainFullStuffBuilder(config).build()
-        analyzer = Analyzer(config)
-        analyzer.verify(dataset)
-
-        dataset = ValFullStuffBuilder(config).build()
-        analyzer = Analyzer(config)
-        analyzer.verify(dataset)
+        TrainFullStuffBuilder(config).build()
+        ValFullStuffBuilder(config).build()
